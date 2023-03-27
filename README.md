251a94a6a1acc333ed96721246ad52aa5f8d79e2
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2023-03-27 10:52:18 +0200

    add readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # ChatGPT CLI
 
-Simple CLI to talk to ChatGPT in the terminal.
+## Description
+
+Simple Python CLI to talk to ChatGPT in the terminal. Using [Rich](https://rich.readthedocs.io/en/stable/index.html) for formatting/syntax highlighting.
+
+## Usage
+
+```bash
+python main.py [-h] [-m MODEL] [-k KEY] [-t THEME]
+```
+
+## Arguments
+
+|short|long|default|help|
+| :--- | :--- | :--- | :--- |
+|`-h`|`--help`||show this help message and exit|
+|`-k`|`--key`|`None`|The OpenAI API key to use. Can also be set through the OPENAI_API_KEY environment variable. Defaults to None|
+|`-m`|`--model`|`gpt-3.5-turbo`|The model to use for the chat. Defaults to gpt-3.5-turbo|
+|`-t`|`--theme`|`gruvbox-dark`|The theme to use for syntax highlighting, available options are the pygments styles. Defaults to gruvbox-dark|
--- chat_gpt/arguments.py
+++ chat_gpt/arguments.py
@@ -1,21 +1,12 @@
 from argparse import ArgumentParser
 
 parser = ArgumentParser(
-    prog="Chat GPT CLI",
-    description="Terminal Interface for Chat GPT",
+    prog="chatgpt",
+    description="Terminal Interface for Chat GPT using rich package as the formatter",
 )
 
 parser.add_argument(
-    "--model",
-    action="store",
-    default="gpt-3.5-turbo",
-    dest="model",
-    required=False,
-    help="The model to use for the chat. Defaults to gpt-3.5-turbo",
-)
-
-parser.add_argument(
-    "--key",
+    "-k", "--key",
     action="store",
     default=None,
     dest="key",
@@ -24,7 +15,16 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--theme",
+    "-m", "--model",
+    action="store",
+    default="gpt-3.5-turbo",
+    dest="model",
+    required=False,
+    help="The model to use for the chat. Defaults to gpt-3.5-turbo",
+)
+
+parser.add_argument(
+    "-t", "--theme",
     action="store",
     default="gruvbox-dark",
     dest="theme",
